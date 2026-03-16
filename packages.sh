457963a5726e769eb2fe1007244bc7149027aad6
Eszee Aech <75526109+Suznyan@users.noreply.github.com>
2026-03-16 19:08:45 +0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,24 +3,34 @@ set -e
 
 USERNAME="$1"
 
-pacman -S --noconfirm 
-adobe-source-han-sans-jp-fonts
-adobe-source-han-serif-jp-fonts
-plasma-meta kde-applications-meta sddm 
-pipewire pipewire-pulse wireplumber 
-fcitx5 fcitx5-mozc fcitx5-configtool 
-htop btop fastfetch tmux 
-openssh zip unzip stow 
-dnscrypt-proxy easyeffects qbittorrent
-krita gimp 
-discord steam obs-studio
-android-tools scrcpy 
-keepassxc lutris syncthing tor
-deadbeef mpv ffmpeg 
+pacman -S --noconfirm \
+plasma-desktop konsole dolphin kate ark spectacle kcalc \
+plasma-systemmonitor plasma-nm plasma-pa powerdevil \
+bluedevil bluez bluez-utils \
+kdeconnect samba kio-extras \
+sddm sddm-kcm polkit-kde-agent \
+ffmpegthumbs kdegraphics-thumbnailers \
+noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu \
+adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts \
+xdg-user-dirs xdg-utils wl-clipboard xclip \
+pipewire pipewire-pulse pipewire-alsa wireplumber \
+fcitx5 fcitx5-mozc fcitx5-configtool \
+htop btop fastfetch tmux \
+openssh zip unzip stow \
+dnscrypt-proxy easyeffects qbittorrent \
+krita gimp \
+discord steam obs-studio \
+android-tools scrcpy \
+keepassxc lutris syncthing tor \
+deadbeef mpv ffmpeg \
 python
 
+systemctl enable NetworkManager
 systemctl enable sddm
+systemctl enable bluetooth
+systemctl enable sshd
 systemctl enable dnscrypt-proxy
+systemctl enable syncthing@$USERNAME
 
 # zram if no swap
 
